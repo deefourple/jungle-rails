e92c6e06d49beab770c6fbd16bed180f131d910b
deefourple <davedehghani@gmail.com>
2017-02-16 23:08:39 +0000

    Created basic user authentication. MUST close all instances of chrome to log out

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
 # before_filter :authorize
   def show
   end
