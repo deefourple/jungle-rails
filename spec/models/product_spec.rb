4fcc871022dd87fb809d95a267e6436f2b1f04ff
deefourple <davedehghani@gmail.com>
2017-02-20 19:22:36 +0000

    Removed dead code on RSpec file

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -29,17 +29,3 @@ RSpec.describe Product, type: :model do
   end
 end
 
-
-# describe '#id' do
-#   it 'should not exist for new records' do
-#     @widget = Widget.new
-#     expect(@widget.id).to be_nil
-#   end
-
-#   it 'should be auto-assigned by AR for saved records' do
-#     @widget = Widget.new
-#     @widget.save!
-
-#     expect(@widget.id).to be_present
-#   end
-# end
