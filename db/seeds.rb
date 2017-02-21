b9ced4b0464b793e22efc8309f8026ea02d5b469
deefourple <davedehghani@gmail.com>
2017-02-21 17:14:11 +0000

    Image issue refactor

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,16 +29,21 @@ cat1 = Category.find_or_create_by! name: 'Apparel'
 cat2 = Category.find_or_create_by! name: 'Electronics'
 cat3 = Category.find_or_create_by! name: 'Furniture'
 
+def seed_image(file_name)
+  File.open(File.join(Rails.root, "/app/assets/images/#{file_name}"))
+end
+
 ## PRODUCTS
 
 puts "Re-creating Products ..."
 
 Product.destroy_all
-
+binding.pry
 cat1.products.create!({
   name:  'Men\'s Classy shirt',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('apparel1.jpg'),
+
+  image: seed_image('apparel1.jpg'),
   quantity: 10,
   price: 64.99
 })
@@ -46,7 +51,7 @@ cat1.products.create!({
 cat1.products.create!({
   name:  'Women\'s Zebra pants',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('apparel2.jpg'),
+  image: seed_image('apparel2.jpg'),
   quantity: 18,
   price: 124.99
 })
@@ -54,7 +59,7 @@ cat1.products.create!({
 cat1.products.create!({
   name:  'Hipster Hat',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('apparel3.jpg'),
+  image: seed_image('apparel3.jpg'),
   quantity: 4,
   price: 34.49
 })
@@ -62,7 +67,7 @@ cat1.products.create!({
 cat1.products.create!({
   name:  'Hipster Socks',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('apparel4.jpg'),
+  image: seed_image('apparel4.jpg'),
   quantity: 8,
   price: 25.00
 })
@@ -70,7 +75,7 @@ cat1.products.create!({
 cat1.products.create!({
   name:  'Russian Spy Shoes',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('apparel5.jpg'),
+  image: seed_image('apparel5.jpg'),
   quantity: 8,
   price: 1_225.00
 })
@@ -78,7 +83,7 @@ cat1.products.create!({
 cat1.products.create!({
   name:  'Human Feet Shoes',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('apparel6.jpg'),
+  image: seed_image('apparel6.jpg'),
   quantity: 82,
   price: 224.50
 })
@@ -87,7 +92,7 @@ cat1.products.create!({
 cat2.products.create!({
   name:  'Modern Skateboards',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('electronics1.jpg'),
+  image: seed_image('electronics1.jpg'),
   quantity: 40,
   price: 164.49
 })
@@ -95,7 +100,7 @@ cat2.products.create!({
 cat2.products.create!({
   name:  'Hotdog Slicer',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('electronics2.jpg'),
+  image: seed_image('electronics2.jpg'),
   quantity: 0,
   price: 26.00
 })
@@ -103,7 +108,7 @@ cat2.products.create!({
 cat2.products.create!({
   name:  'World\'s Largest Smartwatch',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('electronics3.jpg'),
+  image: seed_image('electronics3.jpg'),
   quantity: 32,
   price: 2_026.29
 })
@@ -111,7 +116,7 @@ cat2.products.create!({
 cat3.products.create!({
   name:  'Optimal Sleeping Bed',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('furniture1.jpg'),
+  image: seed_image('furniture1.jpg'),
   quantity: 0,
   price: 3_052.00
 })
@@ -119,7 +124,7 @@ cat3.products.create!({
 cat3.products.create!({
   name:  'Electric Chair',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('furniture2.jpg'),
+  image: seed_image('furniture2.jpg'),
   quantity: 2,
   price: 987.65
 })
@@ -127,7 +132,7 @@ cat3.products.create!({
 cat3.products.create!({
   name:  'Red Bookshelf',
   description: Faker::Hipster.paragraph(4),
-  image: open_asset('furniture3.jpg'),
+  image: seed_image('furniture3.jpg'),
   quantity: 23,
   price: 2_483.75
 })
